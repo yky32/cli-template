ed4a4a77d51b09be51ab0f7742b168cfd682a88c
Wayne Yu <wayneyu.dev@gmail.com>
2022-08-26 18:18:42 +0800

    Add lastUsed.json feature

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
 #!/usr/bin/env node
 
-
-// /*
-// /$$$$$$$$ /$$$$$$ / $$$$$$$ / $$$$$$$$ / $$$$$$ / $$ / $$ / $$$$$$ / $$$$$$$ / $$
-// | $$_____/|_  $$_/| $$__  $$| $$_____/ /$$__  $$| $$  | $$|_  $$_/| $$__  $$|__/
-// | $$        | $$  | $$  \ $$| $$      | $$  \__/| $$  | $$  | $$  | $$  \ $$ /$$  /$$$$$$
-// | $$$$$     | $$  | $$$$$$$/| $$$$$   |  $$$$$$ | $$$$$$$$  | $$  | $$$$$$$/| $$ /$$__  $$
-// | $$__/     | $$  | $$__  $$| $$__/    \____  $$| $$__  $$  | $$  | $$____/ | $$| $$  \ $$
-// | $$        | $$  | $$  \ $$| $$       /$$  \ $$| $$  | $$  | $$  | $$      | $$| $$  | $$
-// | $$       /$$$$$$| $$  | $$| $$$$$$$$|  $$$$$$/| $$  | $$ /$$$$$$| $$ /$$  | $$|  $$$$$$/
-// |__/      |______/|__/  |__/|________/ \______/ |__/  |__/|______/|__/|__/  |__/ \______/
-// */
-
 import chalk from 'chalk';
-import inquirer  from 'inquirer';
+import inquirer from 'inquirer';
 import chalkAnimation from 'chalk-animation';
 import {createSpinner} from 'nanospinner';
 import axios from "axios";
@@ -28,6 +16,7 @@ const LOCALHOST = "localhost";
 // PATH
 const LAST_USED_FILE = '.lastUsed/';
 const RESULT_FILE = '.result/';
+const LAST_USE_FILE = LAST_USED_FILE + 'lastUsed.json';
 
 const data = readFileSync('.config/appConfig.json');
 let appConfig = JSON.parse(data)
@@ -35,10 +24,15 @@ let playerName = 'WY__';
 
 const sleep = (ms = 1000) => new Promise((r) => setTimeout(r, ms));
 
-async function init () {
+function load(filePath) {
+    const _file = readFileSync(filePath);
+    let fileData = JSON.parse(_file)
+    return fileData;
+}
+
+async function init() {
     // load config
-    const _featureMapping = readFileSync('.config/features/featureMapping.json');
-    let featureMapping = JSON.parse(_featureMapping)
+    let featureMapping = load('.config/features/featureMapping.json');
     appConfig.featureMapping = featureMapping
 }
 
@@ -55,7 +49,21 @@ async function welcome() {
     `);
 }
 
-async function getAnswersFromList(rewrite = false, questionName, options = ['']) {
+async function getManyAnswersFromList(rewrite = false, questionName, options = ['']) {
+    let message = 'What is ' + questionName + '?';
+    if (rewrite) message = questionName
+    let answers = await inquirer.prompt(
+        {
+            type: 'checkbox',
+            name: 'player_input',
+            message: message,
+            choices: options
+        }
+    );
+    return answers.player_input;
+}
+
+async function getOneAnswerFromList(rewrite = false, questionName, options = ['']) {
     let message = 'What is ' + questionName + '?';
     if (rewrite) message = questionName
     let answers = await inquirer.prompt({
@@ -67,7 +75,7 @@ async function getAnswersFromList(rewrite = false, questionName, options = [''])
     return answers.player_input;
 }
 
-async function getAnswersFromInput(rewrite = false, questionName, _default = "") {
+async function getOneAnswerFromInput(rewrite = false, questionName, _default = "") {
     let message = 'What is ' + questionName + '?';
     if (rewrite) message = questionName
     let answers = await inquirer.prompt({
@@ -81,28 +89,27 @@ async function getAnswersFromInput(rewrite = false, questionName, _default = "")
     return answers.player_input;
 }
 
-async function askQuestions(questionName) {
+async function askQuestions(questionName, _default) {
     let answers
     let qn = questionName.toUpperCase();
     if (qn === 'URL') {
-        let protocol = await getAnswersFromList(false, 'Protocol', ['http', 'https']);
-
-        let domain = await getAnswersFromInput(false, 'Domain', LOCALHOST);
+        let protocol = await getOneAnswerFromList(false, 'Protocol', ['http', 'https']);
+        let domain = await getOneAnswerFromInput(false, 'Domain', (_default) ? _default : LOCALHOST);
         let portOption
         if (domain === LOCALHOST) {
-            portOption = await getAnswersFromList(false, 'Port', [YES, NO]);
+            portOption = await getOneAnswerFromList(false, 'Port', [YES, NO]);
         } else {
-            portOption = await getAnswersFromList(false, 'Port', [NO, YES]);
+            portOption = await getOneAnswerFromList(false, 'Port', [NO, YES]);
         }
         let port = ''
         if (YES === portOption) {
-            let portInput = await getAnswersFromInput(false, 'Port Number', "8080");
+            let portInput = await getOneAnswerFromInput(false, 'Port Number', (_default) ? _default : "8080");
             port = ':' + portInput;
         }
-        let uri = await getAnswersFromInput(false, 'Uri', "/");
+        let uri = await getOneAnswerFromInput(false, 'Uri', (_default) ? _default : "/");
 
         let url = protocol + '://' + domain + port + uri;
-        let confirm = await getAnswersFromList(true, 'Confirm the URL ?\n' + url, [YES, NO]);
+        let confirm = await getOneAnswerFromList(true, 'Confirm the URL ?\n' + url, [YES, NO]);
         if (YES !== confirm) {
             await askQuestions('URL');
         }
@@ -111,17 +118,17 @@ async function askQuestions(questionName) {
     }
 
     if (qn === 'METHOD') {
-        answers = await getAnswersFromList(false, questionName, ['GET', 'POST']);
+        answers = await getOneAnswerFromList(false, questionName, ['GET', 'POST']);
         return answers;
     }
 
     if (qn === 'NUMBEROFCALL') {
-        answers = await getAnswersFromInput(false, questionName, "1");
+        answers = await getOneAnswerFromInput(false, questionName, (_default) ? _default : "1");
         return answers;
     }
 
     if (qn === 'PAYLOAD') {
-        answers = await getAnswersFromInput(false, questionName, {});
+        answers = await getOneAnswerFromInput(false, questionName, (_default) ? _default : {});
         return answers;
     }
 
@@ -135,8 +142,15 @@ async function askForFeatures() {
         message: 'What you are going to do / check?\n',
         choices: Object.keys(appConfig.features),
     });
-    await handleAnswer(answers.choose_features);
-    return answers.choose_features;
+    let answer = answers.choose_features;
+    if (getFeatureKeyByName(answer) !== 'last') {
+        await handleAnswer(answer);
+    }
+    return answer;
+}
+
+function getFeatureKeyByName(answer) {
+    return appConfig.features[answer];
 }
 
 async function handleAnswer(answer) {
@@ -144,9 +158,9 @@ async function handleAnswer(answer) {
     const spinner = createSpinner('Checking answer...').start();
     await sleep();
 
-    let feature = appConfig.features[answer]
-    if (feature) {
-        appConfig.selected.push(feature)
+    let featureKey = getFeatureKeyByName(answer)
+    if (featureKey) {
+        appConfig.selected.push(featureKey)
         spinner.success({text: `Nice work ${playerName}. That's a legit answer`});
     } else {
         spinner.error({text: `💀💀💀 Game over, thank you ${playerName}!`});
@@ -171,54 +185,85 @@ async function handleAnswer(answer) {
     // print out setting____
     console.log('-- appConfig: ', appConfig)
     // store the actionPlan in json that next time reuse
-    await printAndSaveResult(LAST_USED_FILE + "lastUsed" + '_result_', appConfig.actionPlan, "json")
-
+    appConfig.actionPlan.last = featureKey
+    let timeslot = moment().format('YYYYMMDDHHMMSS');
+    let lastUse = {}
+    lastUse[timeslot] = appConfig.actionPlan
+    await save(LAST_USED_FILE + '/lastUsed.json', lastUse)
 }
 
-async function actionTemplate(actionItem) {
-    let info = appConfig.actionPlan[actionItem];
-    let response = []
-    if (actionItem === 'api') {
+async function actionTemplate(info, featureKey) {
+    let result = {
+        featureKey: featureKey,
+        responses: []
+    }
+    if (featureKey === 'api') {
         for (let i = 1; i <= info.numberOfCall; i++) {
             try {
                 if (info.method === 'GET') {
                     console.log(i, ' ', info.method, 'call --')
-                    response.push((await axios.get(info.url)).data)
+                    result.responses.push((await axios.get(info.url)).data)
                 }
                 if (info.method === 'POST') {
                     let data = JSON.parse(info.payload);
                     console.log(i, ' ', info.method, 'call -- with ', data)
-                    response.push((await axios.post(info.url, data)).data);
+                    result.responses.push((await axios.post(info.url, data)).data);
                 }
-            } catch (e){
+            } catch (e) {
                 console.log(e)
             }
         }
     }
-    return response
+    return result
+}
+
+
+async function handleLastAction() {
+    let lastActionConfig = load(LAST_USE_FILE);
+    console.log(lastActionConfig)
+    let historyKey = await getOneAnswerFromList(true, "Which history?", Object.keys(lastActionConfig))
+    let config = lastActionConfig[historyKey];
+    let info = config[config.last]
+
+    // do you need edit the config?
+    let isEdit = await getOneAnswerFromList(true, "Do you need edit?", [NO, YES]);
+    if (YES === isEdit) {
+        let editList = await getManyAnswersFromList(true, "Which gonna edit?", Object.keys(config[config.last]));
+        for (const field of editList) {
+            let newFieldValue = await askQuestions(field, info[field]);
+            console.log(newFieldValue)
+        }
+    }
+
+    return await actionTemplate(info, config.last);
 }
 
 async function takeAction(actionItem) {
-    console.log('-- TakeAction____', actionItem)
-    return await actionTemplate(actionItem);
+    let featureKey = getFeatureKeyByName(actionItem)
+    if (featureKey === 'last') {
+        return handleLastAction();
+    }
+    let info = appConfig.actionPlan[featureKey];
+    console.log('-- TakeAction____', info)
+    return await actionTemplate(info, featureKey);
 }
 
 function save(fileName, response) {
-    writeFileSync(fileName, response)
+    writeFileSync(fileName, JSON.stringify(response))
 }
 
-async function printAndSaveResult(fileName, responses, fileType = "json") {
+async function printAndSaveResult(fileName = "", responses, fileType = "json") {
     const fileTypeMap = {
         json: ".json",
         txt: ".text"
     }
-    fileName = fileName + moment().format('YYYYMMDDHHMMSS') + "__"+ fileTypeMap[fileType];
+    fileName = fileName + moment().format('YYYYMMDDHHMMSS') + "__" + fileTypeMap[fileType];
     if (Array.isArray(responses)) {
         for (const response of responses) {
-            save(fileName, JSON.stringify(response))
+            save(fileName, responses)
         }
     } else {
-        save(fileName, JSON.stringify(responses))
+        save(fileName, responses)
     }
 }
 
@@ -226,11 +271,11 @@ async function run() {
     await init()
     await welcome()
     let answer = await askForFeatures()
-    let featureKey = appConfig.features[answer];
-    let responses = await takeAction(featureKey)
-    await printAndSaveResult(RESULT_FILE + featureKey + '_result_', responses, "json")
+    let result = await takeAction(answer)
+    await printAndSaveResult(RESULT_FILE + result.featureKey + '_result_', result.responses, "json")
 }
 
 // Run it with top-level await
 console.clear();
-run().then(r => {});
+run().then(r => {
+});
